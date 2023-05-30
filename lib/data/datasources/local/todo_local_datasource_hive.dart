488a305b1a964bd924b9b0e07c3f89bd89ce3f97
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-05-29 19:03:36 -0500

    work on progress

--- lib/data/datasources/local/todo_local_datasource_hive.dart
+++ lib/data/datasources/local/todo_local_datasource_hive.dart
@@ -1,48 +1,30 @@
+import 'package:f_todo_app_database/domain/entities/todo_hive.dart';
 import 'package:hive/hive.dart';
 import 'package:loggy/loggy.dart';
 
-class UserLocalDataSourceHive {
-  /*
-  addUser(RandomUser user) {
-    Hive.box('users').add(UserDB(
-        gender: user.gender,
-        name: user.name,
-        country: user.city,
-        email: user.email,
-        picture: user.picture));
+import '../../../domain/entities/todo.dart';
+
+class TodoLocalDataSourceHive {
+  addTodo(Todo todo) async {
+    TodoHive todoHive = TodoHive.fromTodo(todo: todo);
+    Hive.box('todos').add(todoHive);
   }
 
-  Future<List<RandomUser>> getAllUsers() async {
-    return Hive.box('users').values.map((e) {
-      return RandomUser(
-          id: e.key,
-          name: e.name,
-          city: e.country,
-          email: e.email,
-          picture: e.picture,
-          gender: e.gender);
-    }).toList();
+  Future<List<Todo>> getAllTodos() async {
+    return Hive.box('todos').values.map((TodoHive e) => e.toTodo()).toList();
   }
 
   deleteAll() async {
     logInfo("Deleting all from database");
-    await Hive.box('users').clear();
+    await Hive.box('todos').clear();
   }
 
-  deleteUser(index) async {
-    await Hive.box('users').deleteAt(index);
+  deleteTodo(index) async {
+    await Hive.box('todos').deleteAt(index);
   }
 
-  updateUser(RandomUser user) async {
-    logInfo("Updating entry $user");
-    await Hive.box('users').putAt(
-        user.id!,
-        UserDB(
-            gender: user.gender,
-            name: user.name,
-            country: user.city,
-            email: user.email,
-            picture: user.picture));
+  updateTodo(Todo todo) async {
+    TodoHive todoHive = TodoHive.fromTodo(todo: todo);
+    await Hive.box('todos').putAt(todo.id!, todoHive);
   }
-  */
 }
