556a2ae5cf5ef0a2cb576f9bb711aa726725bb05
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-05-30 18:10:59 -0500

    started widget testing for homepage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,9 +5,18 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:f_todo_app_database/domain/entities/todo.dart';
+import 'package:f_todo_app_database/ui/controllers/todo_controller.dart';
+import 'package:f_todo_app_database/ui/pages/home_page.dart';
 import 'package:f_todo_app_database/ui/widget/new_todo_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
+
+class MockTodoController extends GetxService
+    with Mock
+    implements TodoController {}
 
 void main() {
   testWidgets('NewTodoDialog Test', (WidgetTester tester) async {
@@ -29,5 +38,48 @@ void main() {
 
     // Verify that the dialog is dismissed and the todo is returned
     expect(find.text('New todo'), findsNothing);
+  });
+
+  testWidgets('HomePage Test', (WidgetTester tester) async {
+    // Create a mock instance of the TodoController
+    final mockTodoController = MockTodoController();
+
+    // Inject the mock instance into the Get dependency management system
+    Get.put<TodoController>(mockTodoController);
+
+    // Build the HomePage widget
+    await tester.pumpWidget(const MaterialApp(home: HomePage()));
+
+    // Verify that the initial state is correct
+    expect(find.text('Todo App'), findsOneWidget);
+    expect(find.byKey(const Key('deleteAllButton')), findsOneWidget);
+    expect(find.byKey(const Key('floatingActionButton')), findsOneWidget);
+
+    // Simulate tapping the delete all button
+    await tester.tap(find.byKey(const Key('deleteAllButton')));
+    verify(mockTodoController.removeAll()).called(1);
+
+    // Simulate tapping the floating action button
+    await tester.tap(find.byKey(const Key('floatingActionButton')));
+    await tester.pumpAndSettle();
+
+    // Verify that the NewTodoDialog is shown
+    expect(find.byKey(const Key('newTodoDialog')), findsOneWidget);
+
+    // Simulate adding a todo through the dialog
+    final todo = Todo(
+      title: 'Test Todo',
+      body: 'Test Body',
+      completed: 0,
+      type: TodoType.DEFAULT,
+    );
+    when(mockTodoController.addItem(any)).thenAnswer((_) => null);
+    await tester.tap(find.byKey(const Key('addButtonTodoDialog')));
+    await tester.pumpAndSettle();
+
+    // Verify that the todo is added to the todo list
+    verify(mockTodoController.addItem(todo)).called(1);
+
+    // TODO: Add additional tests for verifying the behavior of the todo list and item widgets
   });
 }
